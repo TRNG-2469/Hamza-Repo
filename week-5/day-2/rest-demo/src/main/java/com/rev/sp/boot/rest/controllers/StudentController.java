fd8f3accd1b37b743f66bcb1066af9bab75224b0
Mo <hamzaniaz9856@gmail.com>
2026-08-20 12:21:32 -0400

    Code along Spring REST

--- week-5/day-2/rest-demo/src/main/java/com/rev/sp/boot/rest/controllers/StudentController.java
+++ week-5/day-2/rest-demo/src/main/java/com/rev/sp/boot/rest/controllers/StudentController.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1")
 public class StudentController {
     private final StudentService studentService;
